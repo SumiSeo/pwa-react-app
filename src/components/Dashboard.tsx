c2e2b22f1ca24ec57dc25809459044376fa1c6c8
Sumi Seo <equepem@gmail.com>
2021-11-17 23:07:13 +0900

    graphql add

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,21 +1,44 @@
 import React from "react";
-const Dashboard: React.FC = () => {
+import { useQuery } from "@apollo/client";
+import { QUERY_EXPENSES } from "../queries/queries";
+interface QueryExpense {
+  amount: number;
+  description: string;
+  id: number;
+  title: string;
+}
+
+interface QueryExpenseData {
+  queryExpense: QueryExpense[];
+}
+
+const Dashboard = () => {
+  const { loading, data } = useQuery<QueryExpenseData>(QUERY_EXPENSES);
+  if (!loading) {
+    console.log("data", data);
+    console.log("query", data);
+  }
+
   return (
     <div className="dashboard">
-      <ul>
-        <li className="dashboard__list">
-          <span>Expense</span>
-          <p>$43</p>
-        </li>
-        <li className="dashboard__list">
-          <span>Income</span>
-          <p>$20</p>
-        </li>
-        <li className="dashboard__list">
-          <span>Total</span>
-          <p>-$23</p>
-        </li>
-      </ul>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <ul>
+          <li className="dashboard__list">
+            <span>Expense</span>
+            <p>{data && data.queryExpense}</p>
+          </li>
+          <li className="dashboard__list">
+            <span>Income</span>
+            <p>$20</p>
+          </li>
+          <li className="dashboard__list">
+            <span>Total</span>
+            <p>-$23</p>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
